71bff3b3759f0a9ae745b6a97be1a19c323bdba9
rittbys <rittbys@users.noreply.github.com>
2016-03-26 15:38:59 -0500

    Fixed transitions

--- _site/scripts/index.js
+++ _site/scripts/index.js
@@ -1,8 +1,8 @@
 (function () {
 
   function init() {
-    document.getElementById('dl-btn').className = "top-btn shadow white";
-    document.getElementById('src-btn').className = "top-btn shadow white";
+    document.getElementById('blog-btn').className = "top-btn shadow white";
+    document.getElementById('rev-btn').className = "top-btn shadow white";
     document.getElementById('ytapp-btn').className = "top-btn shadow white";
   };
 

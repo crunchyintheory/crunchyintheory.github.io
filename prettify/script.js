ef85db124b21e4a89d24e3d5647674c3f752ff63
crunchyintheory <crunchyintheory@users.noreply.github.com>
2017-07-11 18:43:29 -0500

    Migrate prettify to new repo

--- prettify/script.js
+++
@@ -1,7 +0,0 @@
-let textArea = document.getElementsByTagName('textarea')[0];
-let pretty = document.getElementsByClassName('prettyprint')[0];
-const prettify = function() {
-    pretty.className = 'prettyprint';
-    pretty.innerText = textArea.value;
-    PR.prettyPrint();
-};
